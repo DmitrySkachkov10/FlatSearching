b60c2272f28e443dec1d71769fe4e19ce58d2121
Burukku1 <dima.dmitry06@gmail.com>
2024-01-23 01:32:55 +0300

    add FeignClientConfiguration file

---
+++ user-service/src/main/java/by/skachkovdmitry/personal_account/config/FeignClientConfiguration.java
@@ -0,0 +1,24 @@
+package by.skachkovdmitry.personal_account.config;
+
+import feign.RequestInterceptor;
+import feign.RequestTemplate;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class FeignClientConfiguration {
+
+    @Value("${your.token.value}")
+    private String token;
+    @Bean
+    public RequestInterceptor requestInterceptor() {
+        return new RequestInterceptor() {
+            @Override
+            public void apply(RequestTemplate template) {
+                // Добавление токена в хэдер запроса
+                template.header("Authorization", "Bearer " + token);
+            }
+        };
+    }
+}
